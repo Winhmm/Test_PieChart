2c18a9b18d7f7cf0319c32453da22b065422d9e6
nguyenthanhphong30052k6-cmyk <nguyethanhphong30052k6@gmail.com>
2025-11-06 07:46:35 +0700

    Test1

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DEMO_PieChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pieChart1 = new LiveCharts.WinForms.PieChart();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.dgvBrandStats = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBrandStats)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pieChart1
+            // 
+            this.pieChart1.Location = new System.Drawing.Point(-67, 57);
+            this.pieChart1.Name = "pieChart1";
+            this.pieChart1.Size = new System.Drawing.Size(900, 650);
+            this.pieChart1.TabIndex = 0;
+            this.pieChart1.Text = "pieChart1";
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(884, 63);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(645, 644);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // dgvBrandStats
+            // 
+            this.dgvBrandStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBrandStats.Location = new System.Drawing.Point(947, 105);
+            this.dgvBrandStats.Name = "dgvBrandStats";
+            this.dgvBrandStats.RowHeadersWidth = 51;
+            this.dgvBrandStats.RowTemplate.Height = 24;
+            this.dgvBrandStats.Size = new System.Drawing.Size(418, 150);
+            this.dgvBrandStats.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1553, 719);
+            this.Controls.Add(this.dgvBrandStats);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.pieChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBrandStats)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.PieChart pieChart1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.DataGridView dgvBrandStats;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEMO_PieChart
+{
+    public partial class Form1 : Form
+    {
+        // Tạo đối tượng DataContext để truy cập database
+        CarDataDataContext db = new CarDataDataContext();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPieChart();
+            LoadColumnChart();
+            LoadBrandStats();
+        }
+        private void LoadPieChart()
+        {
+            // Lấy dữ liệu tổng số lượng bán theo hãng xe bằng LINQ
+            var query = from b in db.BUYs
+                        join c in db.CARs on b.CAR_ID equals c.CAR_ID
+                        group new { b, c } by c.CAR_BRAND into g
+                        select new
+                        {
+                            Brand = g.Key,
+                            Revenue = g.Sum(x => x.b.BUY_QUANTITY * x.c.CAR_PRICE)
+                        };
+
+            // Gắn dữ liệu vào LiveCharts
+            SeriesCollection series = new SeriesCollection();
+            foreach (var item in query)
+            {
+                series.Add(new PieSeries
+                {
+                    Title = item.Brand,
+                    Values = new ChartValues<double> { (double)item.Revenue },
+                    DataLabels = true,
+                    FontSize = 9,
+                    LabelPoint = chartPoint => $"{chartPoint.SeriesView.Title}: {chartPoint.Y:N0} VNĐ"
+                });
+            }
+            pieChart1.Series = series;
+            pieChart1.LegendLocation = LegendLocation.Right;
+        }
+
+        private void LoadColumnChart()
+        {
+            // Lấy dữ liệu bằng LINQ
+            var query = from b in db.BUYs
+                        join c in db.CARs on b.CAR_ID equals c.CAR_ID
+                        group b by c.CAR_BRAND into g
+                        select new
+                        {
+                            Brand = g.Key,
+                            TotalSold = g.Sum(x => x.BUY_QUANTITY)
+                        };
+            cartesianChart1.Hoverable = true;
+            cartesianChart1.DisableAnimations = false;
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                     Title = "Number Of Cars Sold",
+                     Values = new ChartValues<double>(query.Select(x => (double)x.TotalSold)),
+                     DataLabels = true, // Hiện trên đầu cột
+                     LabelPoint = chartPoint => $"{chartPoint.Y} car"
+                }
+            };
+
+            //  tooltip
+            cartesianChart1.DataTooltip = new DefaultTooltip
+            {
+                // Edit for tooltip
+                SelectionMode = TooltipSelectionMode.OnlySender,
+                Background = System.Windows.Media.Brushes.LightYellow,
+                Foreground = System.Windows.Media.Brushes.Black,
+                FontSize = 13,
+                Padding = new System.Windows.Thickness(8)
+            };
+            cartesianChart1.AxisX.Clear();
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title = "Brand",
+                Labels = query.Select(x => x.Brand).ToArray()
+            });
+
+            cartesianChart1.AxisY.Clear();
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Sales Volume",
+                LabelFormatter = value => value.ToString("N0")
+            });
+            cartesianChart1.LegendLocation = LegendLocation.Top;
+            cartesianChart1.DisableAnimations = false; // Slide up
+        }
+        // Hàm tính trung vị
+        private double GetMedian(List<double> numbers)
+        {
+            if (numbers.Count == 0) return 0;
+
+            var sorted = numbers.OrderBy(n => n).ToList();
+            int count = sorted.Count;
+            if (count % 2 == 1)
+                return sorted[count / 2];
+            else
+                return (sorted[count / 2 - 1] + sorted[count / 2]) / 2.0;
+        }
+        private void LoadBrandStats()
+        {
+            // Lấy danh sách hãng xe
+            var brandStats = (from b in db.BUYs
+                              join c in db.CARs on b.CAR_ID equals c.CAR_ID
+                              group new { b, c } by c.CAR_BRAND into g
+                              select new
+                              {
+                                  Brand = g.Key,
+                                  TotalSold = g.Sum(x => x.b.BUY_QUANTITY),
+                                  AveragePrice = g.Average(x => x.c.CAR_PRICE),
+                                  MedianPrice = GetMedian(
+                                      g.Select(x => (double)x.c.CAR_PRICE).ToList())
+                              })
+                              .OrderByDescending(x => x.TotalSold)
+                              .ToList();
+
+            dgvBrandStats.DataSource = brandStats;
+
+            // Highlight hãng bán nhiều nhất
+            if (dgvBrandStats.Rows.Count > 0)
+            {
+                dgvBrandStats.Rows[0].DefaultCellStyle.BackColor = Color.LightGreen;
+                dgvBrandStats.Rows[0].DefaultCellStyle.Font = new Font(dgvBrandStats.Font, FontStyle.Bold);
+            }
+        }
+
+    }
+}
